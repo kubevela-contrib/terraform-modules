4a504d8b9ad1359fac10e6df1d381be4f85a6dfa
Zheng Xi Zhou <zzxwill@gmail.com>
2022-01-28 11:35:05 +0800

    refine variable comment

--- alibaba/rabbitmq/variables.tf
+++ alibaba/rabbitmq/variables.tf
@@ -5,19 +5,19 @@ variable "name" {
 }
 
 variable "instance_type" {
-  description = "The specification of the instance type."
+  description = "The specification of the instance type. Valid values: professional, vip."
   type        = string
   default     = "professional"
 }
 
 variable "max_tps" {
-  description = "The specification of the peak TPS traffic."
+  description = "The specification of the peak TPS traffic. The smallest valid value is 1000 and the largest value is 100,000."
   type        = number
   default     = 1000
 }
 
 variable "queue_capacity" {
-  description = "The specification of the queue capacity."
+  description = "The specification of the queue capacity. The smallest value is 50 and the step size 5."
   type        = number
   default     = 50
 }
@@ -29,7 +29,7 @@ variable "support_eip" {
 }
 
 variable "max_eip_tps" {
-  description = "The specification of the max eip tps."
+  description = "The specification of the max eip tps. It is valid when support_eip is true. The valid value is [128, 45000] with the step size 128"
   type        = number
   default     = 128
 }
@@ -41,7 +41,7 @@ variable "payment_type" {
 }
 
 variable "period" {
-  description = "The specification of the period."
+  description = "The specification of the period. Valid values: 1, 12, 2, 24, 3, 6."
   type        = number
   default     = 1
 }
@@ -59,31 +59,31 @@ variable "instance_id" {
 }
 
 variable "auto_delete_state" {
-  description = "The specification of the auto delete state."
+  description = "Specifies whether the Auto Delete attribute is configured. Valid values: true: The Auto Delete attributeis configured. If the last queue that is bound to an exchange is unbound, the exchange is automatically deleted. false: The Auto Delete attribute is not configured. If the last queue that is bound to an exchange is unbound, the exchange is not automatically deleted."
   type        = bool
   default     = false
 }
 
 variable "exchange_type" {
-  description = "The specification of the exchange type."
-  type        = string
-  default     = "HEADERS"
+  description = "The specification of the exchange type. Valid values: FANOUT, DIRECT, TOPIC, HEADERS"
+type = string
+default = "HEADERS"
 }
 
 variable "internal" {
-  description = "The specification of the internal."
-  type        = bool
-  default     = false
+description = "The specification of the internal."
+type = bool
+default = false
 }
 
 variable "argument" {
-  description = "The specification of the argument."
-  type        = string
-  default     = "x-match:all"
+description = "The specification of the argument."
+type = string
+default = "x-match:all"
 }
 
 variable "binding_type" {
-  description = "The specification of the binding type."
-  type        = string
-  default     = "QUEUE"
+description = "The specification of the binding type. Valid values: EXCHANGE, QUEUE."
+type = string
+default = "QUEUE"
 }
