027121b032bbf54cb12c1872ab393e9dc2caeda9
Zheng Xi Zhou <zzxwill@gmail.com>
2022-03-01 21:47:16 +0800

    SAE namespace

--- alibaba/sae/namespace/main.tf
+++ alibaba/sae/namespace/main.tf
@@ -7,77 +7,23 @@ terraform {
   }
 }
 
-
-resource "random_uuid" "this" {}
-locals {
-  default_description = "Auto created by serverless devs with terraform"
-  default_name        = "terraform-test-${random_uuid.this.result}"
-}
-
-resource "alicloud_sae_application" "manual" {
-  app_name          = var.name
-  deploy            = false
-  app_description   = local.default_description
-  namespace_id      = alicloud_sae_namespace.default.id
-  vswitch_id        = alicloud_vswitch.vsw.id
-  vpc_id            = alicloud_vpc.vpc.id
-  // vswitch_id = "${alicloud_vswitch.vsw.id},${alicloud_vswitch.vsw1.id}"
-  security_group_id = alicloud_security_group.group.id
-  image_url         = "registry-vpc.cn-hangzhou.aliyuncs.com/sae-demo-image/provider:1.0"
-
-  package_type = "Image"
-  timezone     = "Asia/Shanghai"
-  replicas     = var.replicas
-  cpu          = "500"
-  memory       = "2048"
-
-  #  intranet_slb_id = alicloud_slb_load_balancer.slb.id
-  #  intranet {
-  #    https_cert_id = ""
-  #    port = 80
-  #    protocol = "TCP"
-  #    target_port = 8080
-  #  }
-}
-
 resource "alicloud_sae_namespace" "default" {
-  namespace_description = local.default_description
-  namespace_id          = "cn-hangzhou:demo"
-  namespace_name        = local.default_name
-}
-
-resource "alicloud_vpc" "vpc" {
-  vpc_name   = local.default_name
-  cidr_block = "172.16.0.0/12"
-}
-data "alicloud_zones" "default" {}
-
-resource "alicloud_vswitch" "vsw" {
-  vpc_id     = alicloud_vpc.vpc.id
-  cidr_block = "172.16.0.0/21"
-  zone_id    = data.alicloud_zones.default.zones.0.id
-}
-
-#resource "alicloud_slb_load_balancer" "slb" {
-#  load_balancer_name = local.default_name
-#  address_type       = "intranet"
-#  load_balancer_spec = "slb.s2.small"
-#  vswitch_id         = alicloud_vswitch.vsw.id
-#  tags = {
-#    info = "create for internet"
-#  }
-#}
-
-resource "alicloud_security_group" "group" {
-  name   = local.default_name
-  vpc_id = alicloud_vpc.vpc.id
+  namespace_description = var.namespace_description
+  namespace_id          = format("%s:demo", var.region)
+  namespace_name        = var.namespace_name
 }
 
 
-variable "name" {
-  default = "manual"
+variable "region" {
+  description = "Region"
+  type        = string
 }
 
-variable "replicas" {
-  default = 1
+variable "namespace_description" {
+  description = "Namespace Description"
+  default     = "default"
+}
+
+variable "namespace_name" {
+  description = "Namespace Name"
 }
