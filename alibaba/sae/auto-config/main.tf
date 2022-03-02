b09c5fa17af4a0bbcab3947181bea3e7cfe5f7b3
Zheng Xi Zhou <zzxwill@gmail.com>
2022-03-02 10:47:03 +0800

    outputs for SAE auto config application

--- alibaba/sae/auto-config/main.tf
+++ alibaba/sae/auto-config/main.tf
@@ -60,3 +60,13 @@ variable "replicas" {
   description = "The replicas of the application"
   type        = string
 }
+
+output "app_id" {
+  description = "The id of the application"
+  value       = alicloud_sae_application.auto.0.id
+}
+
+output "app_name" {
+  description = "The name of the application"
+  value       = var.app_name
+}
